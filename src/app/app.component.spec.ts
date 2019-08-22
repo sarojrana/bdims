80accd58a5611db5180d5342163b0614664e516a
Saroj Rana <rana.saroj95@gmail.com>
2019-08-22 13:54:45 +0545

    [update] project name changed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,15 +17,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'mean-test-ang'`, async(() => {
+  it(`should have as title 'bdims'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mean-test-ang');
+    expect(app.title).toEqual('bdims');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mean-test-ang!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bdims!');
   }));
 });
